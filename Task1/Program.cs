9dc92630271e9c3b2008694371f2aab56626cda8
Kostya <kostya_zlatan95@mail.ru>
2022-12-11 18:05:59 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine("Напишите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+    if (number[0] == number[4])
+    {
+        if (number[1] == number[3])
+        {
+            Console.WriteLine($"Ваше число: {number} - палиндром.");
+        }
+        else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
+}
+else Console.WriteLine("Ваше число не отвечает требованиям задачи");
